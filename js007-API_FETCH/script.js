02b41609f4f23016c965cf872eec6ef8acd48266
alanbc <alanbc60@gmail.com>
2024-03-22 19:52:49 -0600

    modificacion del js - api_fetch

--- js007-API_FETCH/script.js
+++ js007-API_FETCH/script.js
@@ -11,57 +11,80 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', async () => {
         console.log("click boton: "+clicksBoton);
 
-        const fechaAlmacenadaString = localStorage.getItem('fecha');
-        let fechaAlmacenada;
+        // si es la primera solicitud
+        if(clicksBoton === 0) {
+            await getData(contenedorFecha, thead, tbody);
+            clicksBoton++;
+            mostrarToast("Primera solicitud");
 
-        if (fechaAlmacenadaString) {
-            fechaAlmacenada = new Date(parseInt(fechaAlmacenadaString));
-        } else {
-            console.error("No se encontró ninguna fecha almacenada en el almacenamiento local.");
         }
 
-        const fechaActual = new Date();
-        // calcular el tiempo en 
-        const minutos = (fechaActual.getTime() - fechaAlmacenada.getTime()) / 60000;
-        console.log("minutos:", minutos);
 
-        // si es la primera vez o el minuto es mayor a 1
-        if (clicksBoton === 0 || (minutos > 1 && fechaAlmacenada !== null)) {
-            await getData(contenedorFecha, thead, tbody);            
-            mostrarToast("Primera solicitud o soli mayor a 1 minuto");
+        else{
+            
+            const fechaAlmacenadaString = localStorage.getItem('fecha');
+            let fechaAlmacenada;
+
+            if (fechaAlmacenadaString) {
+                fechaAlmacenada = new Date(parseInt(fechaAlmacenadaString));
+            } 
+            
+            else {
+                console.error("No se encontró ninguna fecha almacenada en el almacenamiento local.");
+            }
+
+            const fechaActual = new Date();
+            const minutos = (fechaActual.getTime() - fechaAlmacenada.getTime()) / 60000;
+            console.log("minutos:", minutos);
+
+            // si es la primera vez o el minuto es mayor a 1
+            if ( (clicksBoton > 0 && minutos > 1 && fechaAlmacenada !== null) ) {
+                await getData(contenedorFecha, thead, tbody);            
+                mostrarToast("solicitud mayor a 1 minuto se hará de nuevo la solicitud");
+            }
+
+                    // comparar las fechas de la primera solicitud con la segunda
+            else if(clicksBoton > 0 && minutos <= 1) {
+
+                console.log("Segunda vez, menor a 1 minuto");
+
+                contenedorFecha.innerHTML = ``;
+                const textoFecha = document.createElement('p');
+                textoFecha.innerHTML = `Fecha: ${fechaActual.toLocaleString()}`
+                contenedorFecha.appendChild(textoFecha);
+
+                const dataLocalStorage = JSON.parse(localStorage.getItem('data'));
+                console.log("data del local storage: ", dataLocalStorage);
+                tbody.innerHTML = '';
+
+                setTimeout(() => {
+                    for (let i = 0; i < dataLocalStorage.data.length; i++) {
+                        const row = document.createElement('tr');
+                        row.innerHTML = `
+                            <td>${dataLocalStorage.data[i].id}</td>
+                            <td>${dataLocalStorage.data[i].first_name}</td>
+                            <td>${dataLocalStorage.data[i].last_name}</td>
+                            <td>${dataLocalStorage.data[i].email}</td>
+                            <td><img class="img-fluid rounded-circle" src="${dataLocalStorage.data[i].avatar}" alt="${dataLocalStorage.data[i].first_name} ${dataLocalStorage.data[i].last_name}"></td>
+                        `;
+                        tbody.appendChild(row);
+                    }
+                }, 3000);
+
+                // agregar una notificacion toast de bootsrap
+                mostrarToast("Solcitud menor a 1 min - leyendo del local storage");
+            };
+
         }
 
-        // comparar las fechas de la primera solicitud con la segunda
-        else if(clicksBoton > 0 && fechaAlmacenada !== null && minutos <= 1) {
 
-            console.log("Segunda vez, menor a 1 minuto");
 
-            contenedorFecha.innerHTML = ``;
-            const textoFecha = document.createElement('p');
-            textoFecha.innerHTML = `Fecha: ${fechaActual.toLocaleString()}`
-            contenedorFecha.appendChild(textoFecha);
 
-            const dataLocalStorage = JSON.parse(localStorage.getItem('data'));
-            console.log("data del local storage: ", dataLocalStorage);
-            tbody.innerHTML = '';
 
-            setTimeout(() => {
-                for (let i = 0; i < dataLocalStorage.data.length; i++) {
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${dataLocalStorage.data[i].id}</td>
-                        <td>${dataLocalStorage.data[i].first_name}</td>
-                        <td>${dataLocalStorage.data[i].last_name}</td>
-                        <td>${dataLocalStorage.data[i].email}</td>
-                        <td><img class="img-fluid rounded-circle" src="${dataLocalStorage.data[i].avatar}" alt="${dataLocalStorage.data[i].first_name} ${dataLocalStorage.data[i].last_name}"></td>
-                    `;
-                    tbody.appendChild(row);
-                }
-            }, 3000);
 
-            // agregar una notificacion toast de bootsrap
-            mostrarToast("Solcitud menor a 1 min - leyendo del local storage");
-        };
+
+
+
 
         clicksBoton++;
     });
